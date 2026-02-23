95a0850a4a21637ae1731af70ce4acef0ef2a869
Senechal jf <jfsenechal@gmail.com>
2026-02-23 15:47:59 +0100

    Load Montserrat font via Google Fonts and optimize font delivery with preconnect links.

--- header.php
+++ header.php
@@ -15,6 +15,9 @@ $locale = 'fr';
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="profile" href="https://gmpg.org/xfn/11">
         <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() ?>/assets/images/favicon.png"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap">
         <?php wp_head(); ?>
     </head>
 
