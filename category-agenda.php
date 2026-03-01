8fb00c6691189d5804f0bbc54e12b7549d3fdf20
jfsenechal <jfsenechal@gmail.com>
2026-03-01 15:24:19 +0100

    Add category-based filtering to `agenda.html.twig` and refactor events rendering
    Introduced category filters with `agendaFilter()` in `agenda.html.twig`, enabling category-based event filtering. Moved events display logic to a new `_events.html.twig` template and updated `category-agenda.php` to include localized filter labels and structured event data in JSON format for improved client-side interaction.

--- category-agenda.php
+++ category-agenda.php
@@ -2,7 +2,8 @@
 
 namespace VisitMarche\TheWo;
 
-use VisitMarche\ThemeWp\Inc\CategoryMetaData;
+use VisitMarche\ThemeWp\Dto\CommonItem;
+use VisitMarche\ThemeWp\Lib\LocaleHelper;
 use VisitMarche\ThemeWp\Lib\Twig;
 use VisitMarche\ThemeWp\Repository\PivotRepository;
 
@@ -14,13 +15,31 @@ $category = get_category($cat_ID);
 $pivotRepository = new PivotRepository();
 $events = $pivotRepository->loadEvents();
 
-$image =  get_template_directory_uri() . '/assets/tartine/bg_events.png';
+$locale = LocaleHelper::getSelectedLanguage();
+$image = get_template_directory_uri().'/assets/tartine/bg_events.png';
 $filters = [];
+foreach ($events as $event) {
+    foreach ($event->eventCategories as $urn => $specification) {
+        if (!isset($filters[$urn])) {
+            $filters[$urn] = [
+                'urn' => $urn,
+                'name' => $specification->getLabelByLang($locale) ?? $urn,
+            ];
+        }
+    }
+}
+$filters = array_values($filters);
+
+$eventsData = [];
+foreach ($events as $event) {
+    $eventsData[] = CommonItem::createFromOffer($event);
+}
 
 Twig::renderPage(
     '@Visit/agenda.html.twig',
     [
         'events' => $events,
+        'eventsJson' => json_encode($eventsData, JSON_THROW_ON_ERROR),
         'category' => $category,
         'name' => $category->name,
         'nameBack' => '',
